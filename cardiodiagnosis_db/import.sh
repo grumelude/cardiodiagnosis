2d16f74da31421a39e98d9f674837b9c5213162b
Jordi Fontanals <grumelude@gmail.com>
2014-03-08 11:22:50 +0100

    ficheros de exportacion

---
+++ cardiodiagnosis_db/import.sh
@@ -0,0 +1,6 @@
+echo "haciendo backup de data a data_old"
+mv -f ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data/ ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data_old
+echo "importando nuevo data"
+cp -rf data ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/
+echo "importando dump de bd"
+mysql -u lportal --password=lportal  cardiodiagnosis < cardiodiagnosis.dump
