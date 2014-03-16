02ae1714b9fa7700a53e112022a280d028132123
jordi fontanals <grumelude@gmail.com>
2014-03-16 12:58:53 +0100

    update

--- cardiodiagnosis_db/export.sh
+++ cardiodiagnosis_db/export.sh
@@ -2,5 +2,5 @@ echo "borrando data"
 rm -rf data/
 echo "exportando nuevo data"
 cp -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data data
-echo "importando dump de bd"
+echo "exportando dump de bd"
 mysqldump -u lportal --password=lportal  cardiodiagnosis > cardiodiagnosis.dump
