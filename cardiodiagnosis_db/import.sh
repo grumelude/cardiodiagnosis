4e2df075e89c36aab7a8ddfa196c54df3761d006
Jordi Fontanals <grumelude@gmail.com>
2014-03-19 19:28:48 +0100

    update

--- cardiodiagnosis_db/import.sh
+++ cardiodiagnosis_db/import.sh
@@ -1,4 +1,5 @@
 echo "haciendo backup de data a data_old"
+rm -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data_old/
 mv -f ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data/ ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/data_old
 echo "importando nuevo data"
 cp -rf data ~/liferay/cardiodiagnosis/liferay-portal-6.2.0-ce-ga1/
