bb4fe11b391ebae69b13fc39fa4691d2b7310998
jordi <grumelude@gmail.com>
2016-02-12 23:27:59 +0100

    update

--- cardiodiagnosis_db/import.sh
+++ cardiodiagnosis_db/import.sh
@@ -1,7 +1,7 @@
 echo "haciendo backup de data a data_old"
-rm -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga2/data_old/
-mv -f ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga2/data/ ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga2/data_old
+rm -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data_old/
+mv -f ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data/ ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data_old
 echo "importando nuevo data"
-cp -rf data ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga2/
+cp -rf data ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/
 echo "importando dump de bd"
 mysql -u lportal --password=lportal  cardiodiagnosis < cardiodiagnosis.dump
