ce56e422a2c0988187a0797fd4185915f9b5a6ff
jordi fontanals <grumelude@gmail.com>
2016-02-13 17:52:13 +0100

    q#	suprimit:         cardiodiagnosis_db/data/lucene/0/write.lock

--- cardiodiagnosis_db/export.sh
+++ cardiodiagnosis_db/export.sh
@@ -1,6 +1,6 @@
 echo "borrando data temporal para subir"
 rm -rf data/
 echo "exportando nuevo data"
-cp -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga2/data data
+cp -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data data
 echo "exportando dump de bd"
 mysqldump -u lportal --password=lportal  cardiodiagnosis > cardiodiagnosis.dump
