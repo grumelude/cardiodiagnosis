382367efde75a2cefd4fe54fe1ea2d4041535539
jordi <grumelude@gmail.com>
2016-02-24 08:27:51 +0100

    passem a sdk

--- cardiodiagnosis_db/export.sh
+++
@@ -1,6 +0,0 @@
-echo "borrando data temporal para subir"
-rm -rf data/
-echo "exportando nuevo data"
-cp -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data data
-echo "exportando dump de bd"
-mysqldump -u lportal --password=lportal  cardiodiagnosis > cardiodiagnosis.dump
--- cardiodiagnosis_db/import_michel.sh
+++
@@ -1,7 +0,0 @@
-echo "haciendo backup de data a data_old"
-rm -rf ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data_old/
-mv -f ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data/ ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/data_old
-echo "importando nuevo data"
-cp -rf data ~/liferay/cardiodiagnosis/liferay-portal-6.2-ce-ga6/
-echo "importando dump de bd"
-mysql -h 172.26.0.12 -u lportal --password=lportal  cardiodiagnosis < cardiodiagnosis.dump
